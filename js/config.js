bc77fdc506bc62679d5adf4cdd44797938515390
Juan Daniel Bustos Velasquez <123023787+jdaniel9@users.noreply.github.com>
2026-07-15 09:52:49 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ================================================================
 
 // ► CONFIGURACIÓN — pega aquí tu URL de Apps Script
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzsQ-GMfOMFIq9T13cjrFY7nCtEa-YGVA1R6-aF2rgKkTOWo-TnP9mMGZlivjJKGS8/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwnBgAAvMqpP9fP1XkTZ6YEuwmW2SByFpAYweSNSaVHdho379zSArNvUxeSVDTs_ID9/exec";
 
 // Rutas de imágenes (archivos locales en /img/)
 const IMG_MAPA  = 'img/mapa.png';
